a0eb3d663f1d61ecdae762f00f656460560039ea
JavierIbarra <jaibarra1@miuandes.cl>
2019-09-03 19:49:01 -0400

    make

--- Makefile
+++ Makefile
@@ -1,1 +1,28 @@
-# TODO: Crear el Makefile...
+CC = g++
+CFLAGS = -Wall
+
+all: client	#server
+
+client: client.o
+	$(CC) $(CFLAGS) -o client client.o
+
+server: server.o
+	$(CC) $(CFLAGS)  -o server server.o
+
+client.o: client.cpp util.h
+	$(CC) -c client.cpp
+
+server.o: server.cpp util.h
+	$(CC) -c server.cpp
+
+srun: server
+	./server
+
+crun: client
+	./client
+
+runall: server client
+	./server & ./client
+
+clean:
+	rm -f server client *.o
--- server.cpp
+++ server.cpp
@@ -10,11 +10,11 @@ using namespace std;
 // Almacenamiento KV
 KVStore db;
 
-int main(int argc, char** argv) {	
-	
+int main(int argc, char** argv) {
+
 	int sflag = 0;
 	int opt;
-	
+
 	// Procesar opciones de linea de comando
     while ((opt = getopt (argc, argv, "s:")) != -1) {
         switch (opt)
@@ -25,29 +25,29 @@ int main(int argc, char** argv) {
 				break;
 			default:
 				return EXIT_FAILURE;
-          }	    	
+          }
     }
-	
+
 	// Uso elemental del almacenamiento KV:
-	
+
 	// Creamos un arreglo de bytes a mano
 	byte data[] = { 0x01, 0x01, 0x01, 0x01, 0x01 };
 
 	// Luego un vector utilizando el arreglo de bytes
 	vector<byte> vdata(data, data + sizeof(data));
-	
+
 	// Creamos el valor
 	Value val = { 5, vdata };
-	
+
 	// Insertamos un par clave, valor directamente
 	// en el mapa KV
-	
+
 	// Nota: Debiera diseñarse una solución más robusta con una interfaz
 	// adecuada para acceder a la estructura.
 	db.insert(std::pair<unsigned long, Value>(1000, val));
-		
+
 	// Imprimir lo que hemos agregado al mapa KV.
 	cout << db[1000].size << " " << (int) db[1000].data[0] << endl;
-	
+
 	return 0;
 }
